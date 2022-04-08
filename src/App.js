49908d094204456a5f5ddf473c2f498a2ee4d2da
Julie <93963483+juliefruit@users.noreply.github.com>
2022-04-08 12:31:14 -0700

    Added GitHub url

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <h1>
         React Weather App
       </h1>
+      <footer>
+        This project was coded by {" "}
+        <a href='https://www.instagram.com/juliefruit_/' target="_blank">
+          Julie Wright
+          </a> & is{" "}
+        <a href='https://github.com/juliefruit/react-weather-search' target="_blank">
+            open sourced on GitHub!
+          </a>
+      </footer>
     </div>
   );
 }
-
-export default App;
